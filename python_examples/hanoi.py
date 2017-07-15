dc3adf2bad19d7e71ca3c308aabbd2e489b9a99c
Vito <vitoallstarz@gmail.com>
2017-07-15 01:59:55 -0400

    have fun

--- python_examples/hanoi.py
+++ python_examples/hanoi.py
@@ -4,8 +4,7 @@ __author__ = 'dante'
 def hanoi(numero_discos: int, inicio=1, fin=3) -> None:
     if(numero_discos):
         hanoi(numero_discos-1, inicio, 6-inicio-fin)
-        print("Mueve el disco %d de la torre %d a la torre %d" %
-             (numero_discos, inicio, fin))
+        print(f"Mueve el disco {numero_discos} de la torre {inicio} a la torre {fin}")
         hanoi(numero_discos-1, 6-inicio-fin, fin)
 
-hanoi(numero_discos=3)
+hanoi(numero_discos=6)
